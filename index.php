6ee7c540805754cffe15947f5eea7c1f7a81dd18
Nyihtet18 <86526867+Nyihtet18@users.noreply.github.com>
2023-12-13 08:02:15 +0630

    Create index.php

---
+++ index.php
@@ -0,0 +1,338 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+ 
+
+   
+
+  
+  
+<title> Scriptphp_MM</title>
+
+<link rel="icon" href="fv.png" type="image/x-icon">
+
+
+ 
+
+  <style type="text/css">
+  #header{
+    background: black;
+    padding: 1px;
+  }
+  #header ul > li{
+    display: inline;
+    list-style-type: none;
+    padding-right: 10px;
+  }
+  #header li a{
+    color: white;
+    text-decoration: none;
+    padding: 5px;
+  }
+  #header li > a:hover{
+    background: white;
+    color: black;
+  }
+ 
+  #content{
+    background: black;
+    min-height: 450px;;
+    padding: 10px;
+  }
+  #footer{
+    background: black;
+    padding: 5px;
+    color: white;
+    text-align: center;
+  }
+  .video-container {
+    float: none;
+    clear: both;
+    width: 100%;
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 25px;
+    height: 0;
+  }
+  .video-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+        
+        
+  </style>
+  
+</head>
+
+<body>
+  
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        if (/Mobi|Android/i.test(navigator.userAgent)) {
+            // If it's a mobile device, redirect users to a mobile-friendly page
+            window.location.href = "warning.html"; // Replace with the URL of your mobile warning page
+        }
+    });
+</script>
+
+
+  
+  
+    
+
+  <div id="header">
+    <ul>
+      <li style="color: white;font-size: 25px;font-weight: bold"><img src="m.png" alt="Description of the larger photo" width="50" height="50"> Scriptphp_MM &raquo;</li>
+      <li><a href="index.php?page=home">Home Earn Big Money</a></li>
+     
+ 
+      <li><a href="youtube">Airdrop Community</a></li>
+      <li><a href="AppForTermux">App For Earn</a></li>
+      <li><a href="live">Happy Live🟢</a></li>
+    </ul>
+  </div>
+  
+<div id="content">
+    
+ 
+    
+
+    <!DOCTYPE html> 
+<html> 
+<head> 
+<style>
+.block {
+  display: block;
+  width: 100%;
+  border: none;
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+}
+
+.block:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.block2 {
+  display: block;
+  width: 100%;
+  border: none;
+  background-color: red;
+  color: white;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+}
+
+.block2:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+
+#myInput {
+  background-image: url('search.png');
+  background-position: 10px 10px;
+  background-repeat: no-repeat;
+  width: 95%;
+  font-size: 20px;
+  padding: 12px 20px 12px 40px;
+  border: 1px solid #ddd;
+  margin-bottom: 12px;
+}
+
+#myTable {
+  table-layout: auto;
+  text-align: center;
+  width: 100%;
+}
+
+#myTable th, #myTable td {
+  text-align: center;
+  padding: 1px;
+}
+
+#myTable tr {
+  border-bottom: 1px solid #ddd;
+}
+
+#myTable tr.header, #myTable tr:hover {
+  background-color: #f1f1f1;
+}
+a:link { text-decoration: none; }
+a:visited { text-decoration: none; }
+a:hover { text-decoration: none; }
+a:active { text-decoration: none; }
+</style>
+
+</head>
+
+<body>
+
+<input type="text" id="myInput" onkeyup="myFunction()" placeholder=" Search Script Name.../" title="Type in a name">
+
+<table id="myTable">
+  <tr class="header">
+    
+      <th >Name</th>
+    <th >script</th>
+    <th >tutorial</th>
+  </tr>
+  <!-- Google Ads -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145280439520932"
+     crossorigin="anonymous"></script>
+
+ 
+ 
+  
+  <script>
+    var s = document.createElement('script');
+    s.src='//omcrobata.com/pfe/current/micro.tag.min.js?z=6675120'+'&sw=/sw-check-permissions-6dec1.js';
+    s.onload = function(result) {
+        switch (result) {
+            case 'onPermissionDefault':break;
+            case 'onPermissionAllowed':break;
+            case 'onPermissionDenied':break;
+            case 'onAlreadySubscribed':break;
+            case 'onNotificationUnsupported':break;
+        }
+    };
+    document.head.appendChild(s);
+</script>
+
+
+
+
+
+
+
+
+
+  <tr>
+    <!-- Table cell containing the name "Agrometacoin" -->
+    <td style="color: #0f0;font-size: 20px;font-weight: bold">Agrometacoin</td>
+
+    <!-- Table cell with a button linking to the temporary download site -->
+    <td>
+      <a id="downloadLink" href="https://durableordinarilyadministrator.com/sqn40ym4?key=d1bd45af1497da99c0a111d1e40811ff" target="_blank">
+        <button type="button" class="block" id="downloadButton" onclick="startTimer('download')">Tap to Download</button>
+      </a>
+    </td>
+
+    <!-- Table cell with a button linking to the temporary view site -->
+    <td>
+      <a id="viewLink" href="https://durableordinarilyadministrator.com/sqn40ym4?key=d1bd45af1497da99c0a111d1e40811ff" target="_blank">
+        <button type="button" class="block2" id="viewButton" onclick="startTimer('view')">Tap to View</button>
+      </a>
+    </td>
+  </tr>
+
+<script>
+  // Function to start the timer
+  function startTimer(buttonType) {
+    // Set initial countdown time
+    let countdownTime = 5;
+
+    // Update buttons every second
+    const countdownInterval = setInterval(function() {
+      // Update download button label
+      if (buttonType === 'download') {
+        document.getElementById('downloadButton').innerText = `Tap to Download (${countdownTime}s)`;
+      }
+
+      // Update view button label
+      if (buttonType === 'view') {
+        document.getElementById('viewButton').innerText = `Tap to View (${countdownTime}s)`;
+      }
+
+      // Decrease countdown time
+      countdownTime--;
+
+      // If countdown reaches 0, redirect and clear interval
+      if (countdownTime < 0) {
+        // Redirect to the original download link
+        if (buttonType === 'download') {
+          document.getElementById('downloadLink').href = 'https://adshnk.com/mYYE2B';
+        }
+
+        // Redirect to the original view link
+        if (buttonType === 'view') {
+          document.getElementById('viewLink').href = 'https://stackoverflow.com/questions/5454779/how-to-convert-php-date-formats-to-gmt-and-vice-versa'; // Replace with the actual original view link
+        }
+
+        // Clear the interval
+        clearInterval(countdownInterval);
+      }
+    }, 1000); // Update every 1 second
+  }
+</script>
+
+
+
+  
+  </tr></table><script>
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+</script>
+
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2145280439520932"
+     crossorigin="anonymous"></script>
+<!-- Step1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2145280439520932"
+     data-ad-slot="6476082895"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+ 
+
+
+  <h1 style="color: white;">Contact Us</h1>
+    <p style="color: white;">You can contact via email: <a style="color: #0f0;" href="mailto:scriptphpmm@gmail.com">Sent to Me</a></p>
+
+</body> 
+</html>  </div>  
+
+</iframe>  
+
+  <div id="footer">
+    Copyright &copy; Scriptphp_MM
+  </div>
+   
+</body>
+</html>
